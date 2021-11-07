19ecfde9fad6dd8b75899310d0784fa24264b654
osama bin junaid <osama.junaid@c3m.io>
2021-11-07 18:37:59 +0530

    a)added rel=noreferrer for <a> tags. b)Removed usused imports

--- src/components/Body/body.tsx
+++ src/components/Body/body.tsx
@@ -1,38 +1,64 @@
-import logo from '../../svgimageFiles/logo.svg';
-import topcircle from '../../svgimageFiles/top-circle.svg';
-import orangecircle from '../../svgimageFiles/leftOrangeCircle.svg';
-import dottedDesign from '../../svgimageFiles/dottedDesign.svg'
-//import { Redirect } from "react-router-dom";
+import logo from "../../svgimageFiles/logo.svg";
+import topcircle from "../../svgimageFiles/top-circle.svg";
+import orangecircle from "../../svgimageFiles/leftOrangeCircle.svg";
+import dottedDesign from "../../svgimageFiles/dottedDesign.svg";
 
-
-import './body.css'
+import "./body.css";
 
 const Body = () => {
-    return (
-        <div className='bodyDiv' >
-            <div className='bodySubDiv'> 
-            <div>
-            <img src={topcircle} className="topCircle" alt="google developer student club" />
+  return (
+    <div className="bodyDiv">
+      <div className="bodySubDiv">
+        <div>
+          <img
+            src={topcircle}
+            className="topCircle"
+            alt="google developer student club"
+          />
 
-                <h1 className='bodyHead'> Welcome to <br /> GDSC SNU </h1> 
-                <p className='bodyText'>Google Developer Student Club (GDSC) is a Google
-                      Developers program for university students to learn 
-                      web, mobile, and google technologies in a peer-to-peer 
-                      learning environment. 
-                </p> 
-                <a className='bodyLink' href="https://gdsc.community.dev/sister-nivedita-university-kolkata/" target="_blank"> Join Us</a>
-                <button className='bodyButton' style={{background: 'white', color: 'blue'}}> Contact us</button>
+          <h1 className="bodyHead">
+            {" "}
+            Welcome to <br /> GDSC SNU{" "}
+          </h1>
+          <p className="bodyText">
+            Google Developer Student Club (GDSC) is a Google Developers program
+            for university students to learn web, mobile, and google
+            technologies in a peer-to-peer learning environment.
+          </p>
+          <a
+            className="bodyLink"
+            href="https://gdsc.community.dev/sister-nivedita-university-kolkata/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            {" "}
+            Join Us
+          </a>
+          <button
+            className="bodyButton"
+            style={{ background: "white", color: "blue" }}
+          >
+            {" "}
+            Contact us
+          </button>
 
-                <br />
-                <img src={dottedDesign} className="dottedDesign" alt="google developer student club" />
-
-             </div>    
-                <img src={logo} className="App-logo" alt="logo" />
-                <img src={orangecircle} className="orangeCircle" alt="google developer student club" />
-            </div>
-            <img src={logo} className="App-logo2" alt="logo" />
+          <br />
+          <img
+            src={dottedDesign}
+            className="dottedDesign"
+            alt="google developer student club"
+          />
         </div>
-    )
-}
+        <img src={logo} className="App-logo" alt="logo" />
+        <img
+          src={orangecircle}
+          className="orangeCircle"
+          alt="google developer student club"
+        />
+      </div>
+      <img src={logo} className="App-logo2" alt="logo" />
+    </div>
+  );
+};
 
-export default Body
+export default Body;
