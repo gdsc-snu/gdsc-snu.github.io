f9951f38166f39a1de0a1332d4824734afeb4c5c
maaz619 <maazahmedlfc@gmail.com>
2022-03-01 11:12:37 +0530

    fix: layout,svgs,buttons overflow issue

--- src/components/Home/FlowDiagram/FlowDiagram.tsx
+++ src/components/Home/FlowDiagram/FlowDiagram.tsx
@@ -1,47 +1,83 @@
-import './FlowDiagram.css'
-import { BodyProps } from '../Body/body';
-import { useState } from 'react';
+import "./FlowDiagram.css";
+import { BodyProps } from "../Body/body";
+import { useState } from "react";
 import { FaGlobe, FaDiscord } from "react-icons/fa";
 import { MdDone } from "react-icons/md";
 
-function FlowDiagram({ setToggleBody }: BodyProps) {
-
-  const [ toggleLink, setToggleLink ] = useState<boolean>(true)
-  const [ discordOn, setdiscordOn ] = useState<boolean>(false)
-  const [ done , setDone ] = useState<boolean>(false)
+const FlowDiagram: React.FC<BodyProps> = ({ setToggleBody }: BodyProps) => {
+  const [toggleLink, setToggleLink] = useState<boolean>(true);
+  const [discordOn, setdiscordOn] = useState<boolean>(false);
+  const [done, setDone] = useState<boolean>(false);
 
   return (
-    <div className='flowDiagramDiv'>
-      <div className='insideDiv'>
-          <div className='joinLinks'>
-                  <FaGlobe className= { toggleLink ?`icons`: `iconsDone`} />
-              <hr className='hr'/>
-                  <FaDiscord className= { discordOn ? `icons`: `iconsDone` }/>
-              <hr className='hr'/>
-              <p onClick={() => {setToggleBody(true) }}>< MdDone className= { done ? `icons` :`iconsDone` }/></p>
-          </div>
+    <div className="flowDiagramDiv">
+      <div className="insideDiv">
+        <div className="joinLinks">
+          <FaGlobe className={toggleLink ? `icons` : `iconsDone`} />
+          <hr className="hr" />
+          <FaDiscord className={discordOn ? `icons` : `iconsDone`} />
+          <hr className="hr" />
+          <p
+            onClick={() => {
+              setToggleBody(true);
+            }}
+          >
+            <MdDone className={done ? `icons` : `iconsDone`} />
+          </p>
+        </div>
 
-          <div className='gifDiv'>
-          </div>
-          { 
-              toggleLink ?
-              <div onClick={() => {setToggleLink(false); setdiscordOn(true)} } >
-              <a className='redirect'  href="https://gdsc.community.dev/accounts/login/?next=/sister-nivedita-university-kolkata/" target="_blank" rel="noreferrer">
+        <div className="gifDiv"></div>
+        <div className="links">
+          {
+            toggleLink ? (
+              <div
+                className="btn-redirects"
+                onClick={(e) => {
+                  setToggleLink(false);
+                  setdiscordOn(true);
+                }}
+              >
+                <a
+                  className="redirect"
+                  href="https://gdsc.community.dev/accounts/login/?next=/sister-nivedita-university-kolkata/"
+                  target="_blank"
+                  rel="noreferrer"
+                >
                   Become a member
-              </a>
-              </div> :
-              discordOn ? 
-              <a className='redirect' href="https://discord.com/invite/c5fEPAsjcg" target="_blank" onClick={ () => {setdiscordOn(false); setDone(true)}} rel="noreferrer">
+                </a>
+              </div>
+            ) : discordOn ? (
+              <div className="btn-redirects">
+                <a
+                  className="redirect"
+                  href="https://discord.com/invite/c5fEPAsjcg"
+                  target="_blank"
+                  onClick={() => {
+                    setdiscordOn(false);
+                    setDone(true);
+                  }}
+                  rel="noreferrer"
+                >
                   Join Discord
-             </a>:
-             done ?
-             <p className='redirect' onClick={() => {setToggleBody(true) }}> You are all set </p>
-             : null
-                /* <button className='closeModal'> CLOSE</button> */
+                </a>
+              </div>
+            ) : done ? (
+              <p
+                className="redirect"
+                onClick={() => {
+                  setToggleBody(true);
+                }}
+              >
+                {" "}
+                You are all set{" "}
+              </p>
+            ) : null
+            /* <button className='closeModal'> CLOSE</button> */
           }
+        </div>
       </div>
     </div>
   );
-}
+};
 
-export default FlowDiagram 
+export default FlowDiagram;
